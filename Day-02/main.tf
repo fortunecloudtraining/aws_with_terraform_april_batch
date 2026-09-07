65e2a58a9df2c7234df12c47050957ab88edb4ca
fortunecloudtraining <fortunecloudtraining@gmail.com>
2026-09-07 15:42:54 +0530

    day-02 tf fctp april batch

--- Day-02/main.tf
+++ Day-02/main.tf
@@ -11,17 +11,3 @@ resource "aws_instance" "web_app" {
   }
 }
 
-output "instace_public_ip" {
-  description = "this for the aws ec2 public ip"
-  value = aws_instance.web_app.public_ip
-}
-
-output "instace_private_ip" {
-  description = "this for the aws ec2 public ip"
-  value = aws_instance.web_app.private_ip
-}
-
-output "instace_private_dns" {
-  description = "this for the aws ec2 public ip"
-  value = aws_instance.web_app.private_dns
-}
